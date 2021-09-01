c64a80d3e7d2803696ccda4b72c1a95e231e2692
Mohamed <m.ahmedalaa001@gmail.com>
2021-09-01 07:52:41 +0200

    section 4 task 2,3

--- section-4/app.js
+++ section-4/app.js
@@ -1,14 +1,37 @@
+// import Pusher from './pusher.js';
+
+var pusher;
+var timerCounter;
+// pusher.sendMessage();
+// pusher.unSubscribe();
+
 const login = (e) => {
   e.preventDefault();
+  const form = document.querySelector('.login-form');
+
+  if (form.group.value.trim() === '' || form.user.value.trim() === '') {
+    return alert('Both group and Username are required');
+  }
+  const id = uuidv4();
+
+  const user = {
+    id,
+    name: form.user.value.trim(),
+  };
+  localStorage.setItem('userId', id);
+
+  pusher = new PusherJS(user);
+  pusher.subscribe(form.group.value.trim());
+  handleTimer();
+
   console.log('login');
   document.getElementById('login-page').style.display = 'none';
   document.getElementById('chat-window').style.display = 'flex';
+  pusher.getChannelInfo();
 };
 
 const logout = () => {
-  console.log('logout');
-  document.getElementById('chat-window').style.display = 'none';
-  document.getElementById('login-page').style.display = 'block';
+  pusher.unSubscribe();
 };
 
 const sendMessage = (e) => {
@@ -22,6 +45,10 @@ const sendMessage = (e) => {
     alert('Cannot send an empty message');
     return;
   }
+
+  pusher.sendMessage(message);
+  // reset counter
+  timerCounter = 59;
 };
 
 const handleChatNewLine = (el) => {
@@ -65,3 +92,16 @@ textElement.addEventListener('keyup', (e) => {
     isAltPressed = false;
   }
 });
+
+const handleTimer = () => {
+  const timer = document.getElementById('timer');
+  timerCounter = 59;
+  timer.innerHTML = `(00:${timerCounter})`;
+  setInterval(() => {
+    if (timerCounter === 0) {
+      pusher.unSubscribe();
+    }
+    timer.innerHTML = `(00:${timerCounter})`;
+    timerCounter--;
+  }, 1000);
+};
--- section-4/pusher.js
+++ section-4/pusher.js
@@ -1,14 +1,110 @@
-class Pusher {
-  constructor() {
-    this.pusher = new Pusher(APP_KEY, {
-      cluster: APP_CLUSTER,
+class PusherJS {
+  constructor(user) {
+    this.pusher = new Pusher('2651c3014d58d8c8017e', {
+      cluster: 'eu',
+      app_id: '1259308',
+      secret: '1f0c5448aaac351a8715',
+      // authEndpoint: '/',
     });
     this.channel = null;
+    this.auth_key = '2651c3014d58d8c8017e';
+    this.app_id = '1259308';
+    this.secret = '1f0c5448aaac351a8715';
+    this.user = user;
   }
 
-  static subscribeUser = () => {};
+  subscribe(channelName) {
+    this.channel = this.pusher.subscribe(channelName);
+    this.bindChannels();
+  }
 
-  static unSubscribeUser = () => {};
+  async unSubscribe() {
+    await this.pusher.unsubscribe(this.channel.name);
+    console.log('logout');
+    document.getElementById('chat-window').style.display = 'none';
+    document.getElementById('login-page').style.display = 'block';
+  }
+
+  async getChannelInfo() {
+    const timestamp = Date.now() / 1000;
+    const signature = CryptoJS.HmacSHA256(
+      `GET\n/apps/${this.app_id}/channels/${this.channel.name}\nauth_key=${this.auth_key}&auth_timestamp=${timestamp}&auth_version=1.0&info=subscription_count`,
+      this.secret
+    );
+
+    const res = await fetch(
+      `https://cors.bridged.cc/https://api-eu.pusher.com/apps/${this.app_id}/channels/${this.channel.name}?auth_key=${this.auth_key}&auth_timestamp=${timestamp}&auth_version=1.0&auth_signature=${signature}&info=subscription_count`,
+      {
+        method: 'GET',
+      }
+    );
+    res.json().then((data) => this.updateUsers(data.subscription_count));
+  }
+
+  signMessage(md5, timestamp) {
+    return CryptoJS.HmacSHA256(
+      `POST\n/apps/${this.app_id}/events\nauth_key=${this.auth_key}&auth_timestamp=${timestamp}&auth_version=1.0&body_md5=${md5}`,
+      this.secret
+    );
+  }
+
+  getMD5(body) {
+    return CryptoJS.MD5(JSON.stringify(body));
+  }
+
+  getURL(md5, signature, timestamp) {
+    return `https://cors.bridged.cc/https://api-eu.pusher.com/apps/${this.app_id}/events?body_md5=${md5}&auth_version=1.0&auth_key=${this.auth_key}&auth_timestamp=${timestamp}&auth_signature=${signature}`;
+  }
+
+  async sendMessage(message) {
+    const body = {
+      data: JSON.stringify({
+        message: `${message}`,
+        user: { id: `${this.user.id}`, name: `${this.user.name}` },
+      }),
+      name: 'client-msg',
+      channel: this.channel.name,
+    };
+
+    const md5 = this.getMD5(body);
+
+    const timestamp = Date.now() / 1000;
+    const signature = this.signMessage(md5, timestamp);
+    await fetch(this.getURL(md5, signature, timestamp), {
+      method: 'POST',
+      body: JSON.stringify(body),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
+
+  bindChannels() {
+    this.channel.bind('client-msg', (data) => this.handleReceiveMessage(data));
+
+    this.channel.bind_global((eventName, data) => {
+      console.log(
+        `The event ${eventName} was triggered with data ${JSON.stringify(data)}`
+      );
+      console.log(this.channel);
+    });
+  }
+
+  updateUsers(count) {
+    document.getElementById('users-count').innerHTML = count;
+  }
+
+  handleReceiveMessage(data) {
+    const chatbox = document.querySelector('.chat-box');
+    const userId = localStorage.getItem('userId');
+
+    chatbox.innerHTML =
+      chatbox.innerHTML +
+      `<div class="${
+        userId === data.user.id ? 'chat-item user' : 'chat-item'
+      }">${userId === data.user.id ? 'You' : data.user.name}: ${
+        data.message
+      }</div> 
+      `;
+  }
 }
-
-module.exports = Pusher;
